02e6b8f1a69a1b40376d26c877f9689a9fa05821
Jakub Riedl <jakub.riedl@bileto.cz>
2015-01-08 05:27:16 +0100

    initial commit with singleton

---
+++ spec/Pattern/SingletonSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\JakubRiedl\Helper\Pattern;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SingletonSpec extends ObjectBehavior
+{
+    function it_should_return_instance()
+    {
+        //@todo singleton test
+    }
+}
